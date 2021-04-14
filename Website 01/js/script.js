ca991939a45975db60b64909698ff338b4c006e5
Happitack <Christianludeking@live.com>
2021-04-14 13:50:15 +0200

    Added additional fuctionality

--- Website 01/js/script.js
+++ Website 01/js/script.js
@@ -1,5 +1,3 @@
-// Username
-
 const inputUsername = document.getElementById("InputUsername");
 const inputPassword = document.getElementById("InputPassword");
 const warningMsg = document.getElementById("WarningMsg");
@@ -44,22 +42,26 @@ if (inputUsername.selected = true) {
     
 }
 
+function validateEmail() {
+    var retVal = false;
+    let rx = /^[a-z]{2,}$/i;
+    let m = mail.value.match(rx);
+
+    if (m = true)
+    {
+        retVal = true;
+    }
+    return retVal;
+}
 
 
+function validatePassword(){
+var retVal = false;
+let rx = /^\w{8,22}$/;
+let m = pwd.value.match(rx)
 
-// btnLogin.addEventListener("click", function (e) {
-//   let usr = InputUsername.value.length;
-
-//   if (usr > 0) {
-//     inputUsername.style.border = ""; } 
-//   else {
-//     inputUsername.style.border = "2px dotted red";
-//     inputUsername.focus();}
-// });
-
-// inputUsername.addEventListener("keyup", function (e) {
-//   if (this.value.length > 1) {
-//     this.style.border = "";}
-//   else input.style.border = "2px dotted red";}
-//   );
-
+if (m = true){
+    retVal = true;
+}
+return retVal;
+}
--- Website 01/js/warning.js
+++ Website 01/js/warning.js
@@ -4,16 +4,24 @@ btnLogin.addEventListener("click", function (e) {
     if (inputUsername.value.length < 1 && inputPassword.value.length < 1) {x = 0}
     else if (inputUsername.value.length < 1 && inputPassword.value.length > 1) {x = 1}
     else if (inputUsername.value.length > 1 && inputPassword.value.length < 1) {x = 2}
+    else if (inputPassword.value.length < 7) {x = 3}
+    else  {x = 4}
     
     switch (x) {
         case 0:
-            text = "Fields can't be empty";
+            text = "Please enter your credentials above";
             break;
         case 1:
             text = "Email can't be empty";
             break;
         case 2:
             text = "Password can't be empty";
+            break;
+        case 3:
+            text = "Password is too short";
+            break;
+        case 4:
+            text = "";
             break;
         default:
             text = ""
